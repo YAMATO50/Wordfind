093b56fd720c3866bb725c6da881127755e4e505
YAMATO50 <noah.kuemmerle@gmail.com>
2023-01-18 17:41:40 +0100

    added functions to find possible words

--- finder.go
+++ finder.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -55,9 +56,58 @@ var reader *bufio.Reader
 func readLine() string {
 	text, _ := reader.ReadString('\n')
 	text = strings.Replace(text, "\n", "", -1)
+	text = strings.Replace(text, "\r", "", -1)
 	return text
 }
 
 func guess(characters string, length int) []string {
-	return []string{}
+	sameLengthWords := mainDatabase.WordLength[length]
+
+	hashes := getPossibleHashes(characters)
+
+	var possibleWords []string
+	for _, hash := range hashes {
+		possibleWords = append(possibleWords, sameLengthWords.ClassifiedWords[hash]...)
+	}
+
+	return possibleWords
+}
+
+func getPossibleHashes(characters string) []uint32 {
+	maxHash := hashWord(characters)
+
+	components := splitHash(maxHash)
+
+	var possibleHashes []uint32
+	for i := 0; i < len(components); i++ {
+		binArray := toBinaryArray(uint32(i) + 1)
+		var hash uint32
+		for j := 0; j < len(binArray); j++ {
+			hash = components[j] * binArray[j]
+		}
+		possibleHashes = append(possibleHashes, hash)
+	}
+	return possibleHashes
+}
+
+func splitHash(maxHash uint32) (components []uint32) {
+	remainders := toBinaryArray(maxHash)
+
+	for i, remainder := range remainders {
+		component := uint32(math.Pow(2, float64(i))) * remainder
+		if component == 0 {
+			continue
+		}
+		components = append(components, component)
+	}
+	return
+}
+
+func toBinaryArray(num uint32) []uint32 {
+	var remainders []uint32
+	for num != 0 {
+		remainders = append(remainders, num%2)
+		num = num / 2
+	}
+	return remainders
 }
