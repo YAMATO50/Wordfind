5402db219a5ab718ae9e8da3a94590bd7186b3c6
YAMATO50 <noah.kuemmerle@gmail.com>
2023-01-19 15:43:11 +0100

    updated description

--- db.go
+++ db.go
@@ -16,7 +16,12 @@ classifiedWords map is indexed by a value computed from the characters contained
 
 the key is computed as follows:
 
-a-z --> 0000, ä --> 0001, ö --> 0010, ü --> 0100, ß --> 1000
+a --> 0b00000000000000000000000000000001
+b --> 0b00000000000000000000000000000010
+...
+ü --> 0b00010000000000000000000000000000
+ß --> 0b00100000000000000000000000000000
+default: 0b10000000000000000000000000000000
 
 Whenever a word contains one of the characters, OR the value with the sum. At the end, the sum is your key
 */
